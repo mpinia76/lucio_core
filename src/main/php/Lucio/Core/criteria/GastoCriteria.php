b33bb6eef586340bf96d14bad5664d2927f7d2f8
Marcos Piñero <marcos.pinero1976@gmail.com>
2021-02-28 11:20:16 -0300

    Agregar gastos a las estadísticas

--- src/main/php/Lucio/Core/criteria/GastoCriteria.php
+++ src/main/php/Lucio/Core/criteria/GastoCriteria.php
@@ -11,120 +11,154 @@ use Cose\criteria\impl\Criteria;
  */
 class GastoCriteria extends Criteria{
 
-	private $fecha;
-	private $fechaDesde;
-	private $fechaHasta;
-	private $concepto;
-	private $fechaVencimientoHasta;
-	private $estadoNotEqual;
-	private $estado;
-	private $observaciones;
-	private $estadosIn;
+    private $fecha;
+    private $fechaDesde;
+    private $fechaHasta;
+    private $concepto;
+    private $fechaVencimientoHasta;
+    private $estadoNotEqual;
+    private $estado;
+    private $observaciones;
+    private $estadosIn;
 
-	private $estadosNotIn;
+    private $estadosNotIn;
+
+    private $mes;
+
+    /**
+     * @return mixed
+     */
+    public function getMes()
+    {
+        return $this->mes;
+    }
+
+    /**
+     * @param mixed $mes
+     */
+    public function setMes($mes)
+    {
+        $this->mes = $mes;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * @param mixed $year
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+    }
+
+    private $year;
 
 
 
 
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
 
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+    }
 
-	public function getFecha()
-	{
-	    return $this->fecha;
-	}
+    public function getFechaDesde()
+    {
+        return $this->fechaDesde;
+    }
 
-	public function setFecha($fecha)
-	{
-	    $this->fecha = $fecha;
-	}
+    public function setFechaDesde($fechaDesde)
+    {
+        $this->fechaDesde = $fechaDesde;
+    }
 
-	public function getFechaDesde()
-	{
-	    return $this->fechaDesde;
-	}
+    public function getFechaHasta()
+    {
+        return $this->fechaHasta;
+    }
 
-	public function setFechaDesde($fechaDesde)
-	{
-	    $this->fechaDesde = $fechaDesde;
-	}
+    public function setFechaHasta($fechaHasta)
+    {
+        $this->fechaHasta = $fechaHasta;
+    }
 
-	public function getFechaHasta()
-	{
-	    return $this->fechaHasta;
-	}
+    public function getConcepto()
+    {
+        return $this->concepto;
+    }
 
-	public function setFechaHasta($fechaHasta)
-	{
-	    $this->fechaHasta = $fechaHasta;
-	}
+    public function setConcepto($concepto)
+    {
+        $this->concepto = $concepto;
+    }
 
-	public function getConcepto()
-	{
-	    return $this->concepto;
-	}
+    public function getFechaVencimientoHasta()
+    {
+        return $this->fechaVencimientoHasta;
+    }
 
-	public function setConcepto($concepto)
-	{
-	    $this->concepto = $concepto;
-	}
+    public function setFechaVencimientoHasta($fechaVencimientoHasta)
+    {
+        $this->fechaVencimientoHasta = $fechaVencimientoHasta;
+    }
 
-	public function getFechaVencimientoHasta()
-	{
-	    return $this->fechaVencimientoHasta;
-	}
+    public function getEstado()
+    {
+        return $this->estado;
+    }
 
-	public function setFechaVencimientoHasta($fechaVencimientoHasta)
-	{
-	    $this->fechaVencimientoHasta = $fechaVencimientoHasta;
-	}
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+    }
 
-	public function getEstado()
-	{
-	    return $this->estado;
-	}
+    public function getEstadoNotEqual()
+    {
+        return $this->estadoNotEqual;
+    }
 
-	public function setEstado($estado)
-	{
-	    $this->estado = $estado;
-	}
+    public function setEstadoNotEqual($estadoNotEqual)
+    {
+        $this->estadoNotEqual = $estadoNotEqual;
+    }
 
-	public function getEstadoNotEqual()
-	{
-	    return $this->estadoNotEqual;
-	}
+    public function getObservaciones()
+    {
+        return $this->observaciones;
+    }
 
-	public function setEstadoNotEqual($estadoNotEqual)
-	{
-	    $this->estadoNotEqual = $estadoNotEqual;
-	}
+    public function setObservaciones($observaciones)
+    {
+        $this->observaciones = $observaciones;
+    }
 
-	public function getObservaciones()
-	{
-	    return $this->observaciones;
-	}
+    public function getEstadosIn()
+    {
+        return $this->estadosIn;
+    }
 
-	public function setObservaciones($observaciones)
-	{
-	    $this->observaciones = $observaciones;
-	}
+    public function setEstadosIn($estadosIn)
+    {
+        $this->estadosIn = $estadosIn;
+    }
 
-	public function getEstadosIn()
-	{
-	    return $this->estadosIn;
-	}
+    public function getEstadosNotIn()
+    {
+        return $this->estadosNotIn;
+    }
 
-	public function setEstadosIn($estadosIn)
-	{
-	    $this->estadosIn = $estadosIn;
-	}
-
-	public function getEstadosNotIn()
-	{
-	    return $this->estadosNotIn;
-	}
-
-	public function setEstadosNotIn($estadosNotIn)
-	{
-	    $this->estadosNotIn = $estadosNotIn;
-	}
+    public function setEstadosNotIn($estadosNotIn)
+    {
+        $this->estadosNotIn = $estadosNotIn;
+    }
 }
--- src/main/php/Lucio/Core/dao/impl/GastoDoctrineDAO.php
+++ src/main/php/Lucio/Core/dao/impl/GastoDoctrineDAO.php
@@ -20,102 +20,124 @@ use Doctrine\ORM\QueryBuilder;
  */
 class GastoDoctrineDAO extends CrudDAO implements IGastoDAO{
 
-	protected function getClazz(){
-		return get_class( new Gasto() );
-	}
+    protected function getClazz(){
+        return get_class( new Gasto() );
+    }
 
-	protected function getQueryBuilder(ICriteria $criteria){
+    protected function getQueryBuilder(ICriteria $criteria){
 
-		$queryBuilder = $this->getEntityManager()->createQueryBuilder();
+        $queryBuilder = $this->getEntityManager()->createQueryBuilder();
 
-		$queryBuilder->select(array('g', 'cg'))
-	   				->from( $this->getClazz(), "g")
-					->leftJoin('g.concepto', 'cg');
+        $queryBuilder->select(array('g', 'cg'))
+            ->from( $this->getClazz(), "g")
+            ->leftJoin('g.concepto', 'cg');
 
-		return $queryBuilder;
-	}
+        return $queryBuilder;
+    }
 
-	protected function getQueryCountBuilder(ICriteria $criteria){
+    protected function getQueryCountBuilder(ICriteria $criteria){
 
-		$queryBuilder = $this->getEntityManager()->createQueryBuilder();
+        $queryBuilder = $this->getEntityManager()->createQueryBuilder();
 
-		$queryBuilder->select('count(g.oid)')
-	   				->from( $this->getClazz(), "g")
-					->leftJoin('g.concepto', 'cg');
+        $queryBuilder->select('count(g.oid)')
+            ->from( $this->getClazz(), "g")
+            ->leftJoin('g.concepto', 'cg');
 
-		return $queryBuilder;
-	}
+        return $queryBuilder;
+    }
 
-	protected function enhanceQueryBuild(QueryBuilder $queryBuilder, ICriteria $criteria){
+    protected function enhanceQueryBuild(QueryBuilder $queryBuilder, ICriteria $criteria){
 
-		$fecha = $criteria->getFecha();
-		if( !empty($fecha) ){
-			$queryBuilder->andWhere( "g.fecha = '" . $fecha->format("Y-m-d") . "'");
-		}
+        $fecha = $criteria->getFecha();
+        if( !empty($fecha) ){
+            $fecha->setTime(0,0,0);
+            $queryBuilder->andWhere( "g.fecha >= '" . $fecha->format("Y-m-d") . "'");
+            $fecha->modify("+1 day");
+            $queryBuilder->andWhere( "g.fecha < '" . $fecha->format("Y-m-d") . "'");
+        }
 
-		$fechaDesde = $criteria->getFechaDesde();
-		if( !empty($fechaDesde) ){
-			$queryBuilder->andWhere( "g.fecha >= '" . $fechaDesde->format("Y-m-d") . "'");
-		}
+        $fechaDesde = $criteria->getFechaDesde();
+        if( !empty($fechaDesde) ){
+            $queryBuilder->andWhere( "g.fecha >= '" . $fechaDesde->format("Y-m-d") . "'");
+        }
 
-		$fechaHasta = $criteria->getFechaHasta();
-		if( !empty($fechaHasta) ){
-			$queryBuilder->andWhere( "g.fecha <= '" . $fechaHasta->format("Y-m-d") . "'");
-		}
+        $fechaHasta = $criteria->getFechaHasta();
+        if( !empty($fechaHasta) ){
+            $queryBuilder->andWhere( "g.fecha <= '" . $fechaHasta->format("Y-m-d") . "'");
+        }
 
-		$concepto = $criteria->getConcepto();
-		if( !empty($concepto) && $concepto!=null){
-			$conceptoOid = $concepto->getOid();
-			if(!empty($conceptoOid))
-				$queryBuilder->andWhere( "cg.oid= $conceptoOid" );
-		}
+        $concepto = $criteria->getConcepto();
+        if( !empty($concepto) && $concepto!=null){
+            $conceptoOid = $concepto->getOid();
+            if(!empty($conceptoOid))
+                $queryBuilder->andWhere( "cg.oid= $conceptoOid" );
+        }
 
-		$estadoNot = $criteria->getEstadoNotEqual();
-		if( !empty($estadoNot) ){
-			$queryBuilder->andWhere( "g.estado != " . $estadoNot );
-		}
+        $estadoNot = $criteria->getEstadoNotEqual();
+        if( !empty($estadoNot) ){
+            $queryBuilder->andWhere( "g.estado != " . $estadoNot );
+        }
 
-		$estado = $criteria->getEstado();
-		if( !empty($estado) ){
-			$queryBuilder->andWhere( "g.estado = " . $estado );
-		}
+        $estado = $criteria->getEstado();
+        if( !empty($estado) ){
+            $queryBuilder->andWhere( "g.estado = " . $estado );
+        }
 
-		$estadosNotIn = $criteria->getEstadosNotIn();
-		if( !empty($estadosNotIn)){
+        $estadosNotIn = $criteria->getEstadosNotIn();
+        if( !empty($estadosNotIn)){
 
-			$oids = implode(",", $estadosNotIn);
+            $oids = implode(",", $estadosNotIn);
 
-			$queryBuilder->andWhere("g.estado not in ($oids)");
-		}
+            $queryBuilder->andWhere("g.estado not in ($oids)");
+        }
 
-		$estadosIn = $criteria->getEstadosIn();
-		if( !empty($estadosIn)){
+        $estadosIn = $criteria->getEstadosIn();
+        if( !empty($estadosIn)){
 
-			$oids = implode(",", $estadosIn);
+            $oids = implode(",", $estadosIn);
 
-			$queryBuilder->andWhere("g.estado in ($oids)");
-		}
+            $queryBuilder->andWhere("g.estado in ($oids)");
+        }
 
-		$fechaVencimientoHasta = $criteria->getFechaVencimientoHasta();
-		if( !empty($fechaVencimientoHasta) ){
-			$queryBuilder->andWhere( "g.fechaVencimiento <= '" . $fechaVencimientoHasta->format("Y-m-d") . "'");
-		}
+        $fechaVencimientoHasta = $criteria->getFechaVencimientoHasta();
+        if( !empty($fechaVencimientoHasta) ){
+            $queryBuilder->andWhere( "g.fechaVencimiento <= '" . $fechaVencimientoHasta->format("Y-m-d") . "'");
+        }
+
+        $mes = $criteria->getMes();
+        if( !empty($mes) ){
+            $emConfig = $this->getEntityManager()->getConfiguration();
+            $emConfig->addCustomDatetimeFunction('YEAR', 'DoctrineExtensions\Query\Mysql\Year');
+            $emConfig->addCustomDatetimeFunction('MONTH', 'DoctrineExtensions\Query\Mysql\Month');
+
+            $queryBuilder->andWhere( "MONTH(g.fecha) =" . $mes->format("m"));
+            $queryBuilder->andWhere( "YEAR(g.fecha) =" . $mes->format("Y"));
+        }
+
+        $year = $criteria->getYear();
+        if( !empty($year) ){
+
+            $emConfig = $this->getEntityManager()->getConfiguration();
+            $emConfig->addCustomDatetimeFunction('YEAR', 'DoctrineExtensions\Query\Mysql\Year');
+
+            $queryBuilder->andWhere( "YEAR(g.fecha) =" . $year->format("Y"));
+        }
 
 
 
-	}
+    }
 
-	protected function getFieldName($name){
+    protected function getFieldName($name){
 
-		$hash = array();
+        $hash = array();
 
-		if( array_key_exists($name, $hash) )
-			return $hash[$name];
-		else{
-			return "g.$name";
-		}
+        if( array_key_exists($name, $hash) )
+            return $hash[$name];
+        else{
+            return "g.$name";
+        }
 
-	}
+    }
 
 
 }
